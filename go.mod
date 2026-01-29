6c86c0e1bea87b5dbf01a4f999c1ba5258b51b39
Nikola Adamov <adamov.nikooo@gmail.com>
2026-01-29 15:14:53 +0300

    Init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/NikoooAds/mylib
+
+go 1.25.5
---
+++ mylib.go
@@ -0,0 +1,11 @@
+package mylib
+
+import "fmt"
+
+func Hello(name string) string {
+	return fmt.Sprintf("Hello, %s!", name)
+}
+
+func Add(a, b int) int {
+	return a + b
+}
